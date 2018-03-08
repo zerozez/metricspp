edb6e3fefbcda08da68315fc61773799ac35ea6b
Petr Orlov <zfmeze@gmail.com>
2018-03-09 01:25:53 +0300

    Update Examples

--- examples/ex_influx.cc
+++ examples/ex_influx.cc
@@ -23,36 +23,31 @@
  * SOFTWARE.
  */
 
-#include <chrono>
-#include <thread>
-#include <memory>
+#include <metricspp/metricspp.hpp>
 
-#include <timestamp-c++.hpp>
+int main(int argc, char** argv) {
+  auto ms = metricspp::create_connection("http://127.0.0.1:8086/write?db=test",
+                                         metricspp::Tag{"server", "server01"},
+                                         metricspp::Tag{"region", "eu-east"});
 
+  // Simple usage with different measurements
+  ms << "eth0" << 53;
 
-using namespace timestamp;
+  // Add custom values
+  ms << "eth1"
+     << "load"
+     << "41"
+     << "ploss" << 41221;
 
+  auto param1 = metricspp::create_measurement("cpu");
+  auto param2 = metricspp::create_measurement("ssd", "sda", "sdb", "sdc");
 
-int main (int argc, char** argv)
-{
-    auto factory = CFactory::create();
+  ms << param1 << 0.92;  // automatic sends value=0.92
 
-    factory->setupConnection("127.0.0.1", 4444, "influx.udp");
-    auto counter = factory->counter<int>("counter1",
-                                {
-                                    {"host", "localhost"},
-                                    {"region", "Imaginaryland"},
-                                    {"cpu", "test-cpu"}
-                                },
-                                "test");
+  ms << param2 << 15.4 << 123.3 << 19.31;  // Fill-up all values
+  ms << param2 << 15.4 << 123.3;           // Fill all excepct last one
 
+  ms << param2 << "sdc" << 1.3 << "sda" << 3.3;  // Fill  only specified values
 
-    for(int i = 0; i < 10000L; i++)
-    {
-        // time operation whatever
-        std::this_thread::sleep_for(std::chrono::milliseconds(i % 4));
-        counter->value(i);
-    }
-
-    return 0;
+  return 0;
 }
